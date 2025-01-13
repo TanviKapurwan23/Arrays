3d1ccd93cb06b649e22097ff2c2f114b44dc8307
Tanvi Kapurwan <125541005+TanviKapurwan23@users.noreply.github.com>
2025-01-13 23:17:43 +0530

    Add files via upload

---
+++ setZero.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+
+
+int main(){
+    int arr[10][10], n, m, markRow;
+
+    
+    for(int i=0; i<n; i++){
+    for(int j=0; j<m; j++){
+        if(arr[i][j] == 0){
+            markRow[i];
+            markRow[j];
+        }
+    }
+}
+
+    return 0;
+}
