a538589c0173593a2b7b42d0f48c4bc883b7c3aa
Tanvi Kapurwan <125541005+TanviKapurwan23@users.noreply.github.com>
2025-01-13 23:17:22 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int n;
+    cout<<"Give me some number"<<endl;
+    cin>>n;
+    for(int i=0; i<n; i++){
+       cout<<"Hello World"<<endl;
+    }
+
+    return 0;
+}
+
+
+// 0 
+// 1 
+// 2
+// 3
+// 4
